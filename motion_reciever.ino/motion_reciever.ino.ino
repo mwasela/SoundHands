f297d3b332cc274c7ccf731576d662699dfde865
michael mwasela <dennysmycall@gmail.com>
2018-10-30 08:21:02 +0300

    More Sensor callibration

---
+++ motion_reciever.ino/motion_reciever.ino.ino
@@ -0,0 +1,93 @@
+//connection for rf receiver
+//GND = GND
+//DATA = 12
+//VCC = 5V
+
+#include <VirtualWire.h>
+//connection for motors
+//motor one
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+// motor two
+int enB = 11;
+int in3 = 7;
+int in4 = 6;
+void setup()
+{
+vw_set_ptt_inverted(true); // Required for DR3100
+vw_set_rx_pin(12);
+vw_setup(4000); // Bits per sec
+//pinMode(13, OUTPUT);
+  // set all the motor control pins to outputs
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+vw_rx_start(); // Start the receiver PLL running
+}
+void loop()
+{
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t buflen = 2;
+if (vw_get_message(buf, &buflen)) // Non-blocking
+{
+  
+  Serial.print(buf[0]) ;
+  Serial.println(buf[1]);
+  if(buf[0]=='f'){
+  Serial.print("forward");
+  Serial.println(buf[1]);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA,255);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enB,255);
+  }
+  if(buf[0]=='b')  {
+  Serial.print("backward");
+  Serial.println(buf[1]);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA,255);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB,255);
+}
+  if(buf[0]=='s'){
+  Serial.print("stop");
+  Serial.println(buf[1]);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  analogWrite(enA,255);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enB,255);
+}
+if(buf[0]=='l'){
+  Serial.print("left");
+  Serial.println(buf[1]);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA,255);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enB,255);
+}
+if(buf[0]=='r'){
+  Serial.print("right");
+  Serial.println(buf[1]);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA,255);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+  analogWrite(enB,255);
+}
+
+}
+}
---
+++ motion_transmitter.ino/motion_transmitter.ino.ino
@@ -0,0 +1,89 @@
+//connection for DNPU6
+//VCC = 3.3/5v
+//GND = GND
+//SCL = 3
+//SDA = 2
+//INT = 7
+
+//connection for transmitter
+//DATA=12
+//VCC=5V
+//GND=GND
+
+#include <VirtualWire.h>
+char *controller;
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+void setup() 
+{
+  pinMode(13,OUTPUT);
+  vw_set_ptt_inverted(true); //
+  vw_set_tx_pin(12);
+  vw_setup(4000);// speed of data transfer Kbps
+  Serial.begin(9600);
+ 
+  Serial.println("Initialize MPU6050");
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop()
+{
+  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calculate Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+    Serial.print(" \nPitch = ");
+    Serial.print(pitch);
+    Serial.print(" Roll = ");
+    Serial.print(roll);
+    
+   if( pitch > 30){
+    controller="f" ;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, HIGH);
+    delay(200);
+     }   
+     else if( pitch < -30){
+    controller="b" ;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, LOW);
+    delay(200);
+     }   
+     else if(roll > 30){
+    controller="r" ;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, HIGH);
+    delay(200);
+     }   
+     else if(roll < -30){
+    controller="l" ;
+    vw_send((uint8_t *)controller, strlen(controller));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, LOW);
+    delay(200);
+     }
+      else {
+      controller="s" ;
+      vw_send((uint8_t *)controller, strlen(controller));
+      vw_wait_tx(); // Wait until the whole message is gone  
+      delay(200); 
+      digitalWrite(13, LOW);
+     } 
+  
+  delay(10);
+}
+
+
